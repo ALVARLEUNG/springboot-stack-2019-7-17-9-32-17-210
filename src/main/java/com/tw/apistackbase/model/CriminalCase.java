610af6150dabf4768767e82bb8e70b0d6b28f4ca
ALVARLEUNG <alvar.leung@qq.com>
2019-07-17 23:19:12 +0800

    [LST]test: add test for findById in AC2

--- src/main/java/com/tw/apistackbase/model/CriminalCase.java
+++ src/main/java/com/tw/apistackbase/model/CriminalCase.java
@@ -8,7 +8,7 @@ public class CriminalCase {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private long id;
+    private Long id;
 
     @Column(nullable = false)
     private String name;
@@ -16,11 +16,11 @@ public class CriminalCase {
     @Column(nullable = false)
     private Long time;
 
-    public long getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(long id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
--- src/test/java/com/tw/apistackbase/repository/CriminalCaseRepositoryTest.java
+++ src/test/java/com/tw/apistackbase/repository/CriminalCaseRepositoryTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 //import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
@@ -22,6 +23,7 @@ public class CriminalCaseRepositoryTest {
 
 
     @Test
+    @DirtiesContext
     public void test_should_return_case_when_create_a_case() {
         //given
         CriminalCase criminalCase = new CriminalCase();
@@ -37,6 +39,7 @@ public class CriminalCaseRepositoryTest {
     }
 
     @Test
+    @DirtiesContext
     public void test_should_throw_exception_when_create_a_case() {
         //given
         CriminalCase criminalCase = new CriminalCase();
@@ -46,6 +49,24 @@ public class CriminalCaseRepositoryTest {
         assertThrows(Exception.class, () -> {
             criminalCaseRepository.saveAndFlush(criminalCase);
         });
+    }
+
+    @Test
+    @DirtiesContext
+    public void test_should_return_case_when_find_the_case_by_id () {
+        //given
+        CriminalCase criminalCase = new CriminalCase();
+        criminalCase.getId();
+        criminalCase.setTime((long) 102011);
+        criminalCase.setName("criminalCase");
+
+        //when
+         criminalCaseRepository.save(criminalCase);
+
+        //then
+        CriminalCase criminalCase1 = criminalCaseRepository.findById(criminalCaseRepository.save(criminalCase).getId()).orElse(null);
+        Assertions.assertNotNull(criminalCase1);
+
     }
 
 
