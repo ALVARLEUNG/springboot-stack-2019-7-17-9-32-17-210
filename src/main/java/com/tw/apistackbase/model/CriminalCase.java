6afe9496b38a59db803c9983b0657d624b26aa8b
ALVARLEUNG <alvar.leung@qq.com>
2019-07-17 23:06:18 +0800

    [LST]feat: add test for AC1

---
+++ src/main/java/com/tw/apistackbase/model/CriminalCase.java
@@ -0,0 +1,42 @@
+package com.tw.apistackbase.model;
+
+
+import javax.persistence.*;
+
+@Entity
+public class CriminalCase {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Long time;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getTime() {
+        return time;
+    }
+
+    public void setTime(Long time) {
+        this.time = time;
+    }
+}
---
+++ src/test/java/com/tw/apistackbase/repository/CriminalCaseRepositoryTest.java
@@ -0,0 +1,54 @@
+package com.tw.apistackbase.repository;
+
+import com.tw.apistackbase.model.CriminalCase;
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+//import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@DataJpaTest
+@RunWith(SpringRunner.class)
+public class CriminalCaseRepositoryTest {
+
+    @Autowired
+    CriminalCaseRepository criminalCaseRepository;
+
+
+    @Test
+    public void test_should_return_case_when_create_a_case() {
+        //given
+        CriminalCase criminalCase = new CriminalCase();
+        criminalCase.getId();
+        criminalCase.setName("criminalCase");
+        criminalCase.setTime((long) 102011);
+
+        //when
+        criminalCaseRepository.save(criminalCase);
+
+        //then
+        Assertions.assertEquals(Long.valueOf(102011), criminalCase.getTime());
+    }
+
+    @Test
+    public void test_should_throw_exception_when_create_a_case() {
+        //given
+        CriminalCase criminalCase = new CriminalCase();
+        criminalCase.getId();
+        criminalCase.setName("criminalCase");
+
+        assertThrows(Exception.class, () -> {
+            criminalCaseRepository.saveAndFlush(criminalCase);
+        });
+    }
+
+
+
+}
+
