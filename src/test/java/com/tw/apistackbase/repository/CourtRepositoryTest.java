24fe6ddec1d152434d7021e74acbc275f7777b8b
ALVARLEUNG <alvar.leung@qq.com>
2019-07-18 07:45:03 +0800

    [LST]test: add test for find court by id

--- src/test/java/com/tw/apistackbase/repository/CourtRepositoryTest.java
+++ src/test/java/com/tw/apistackbase/repository/CourtRepositoryTest.java
@@ -13,6 +13,7 @@ import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -47,6 +48,23 @@ public class CourtRepositoryTest {
         assertThrows(Exception.class, () -> {
             courtRepository.saveAndFlush(new Court());
         });
+    }
+
+    @Test
+    @DirtiesContext
+    public void test_should_return_court_when_find_the_court_by_id () {
+        //given
+        Court court = new Court();
+        court.setName("Court");
+
+
+        //when
+        Court court1 = courtRepository.save(court);
+
+        //then
+        Court result = courtRepository.findById(court1.getId()).orElse(null);
+        Assertions.assertNotNull(result);
+
     }
 
 
